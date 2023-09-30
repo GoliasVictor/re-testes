ce635623363be412796581f70aabfcb9c0e075bb
GoliasVictor <dev.jvsb@gmail.com>
2023-09-29 21:44:03 -0300

    implemented key_down
    Moved update, and implemented replaced key_press by key_down, and centralized the game

--- tetris/src/main.rs
+++ tetris/src/main.rs
@@ -5,43 +5,29 @@ mod logic;
 
 extern crate glium;
 use glium::{
-    glutin::{event, event::KeyboardInput, event_loop},
+    glutin::{event, event::{KeyboardInput, ElementState, VirtualKeyCode}, event_loop},
     Surface,
 };
 use std::time;
 
 pub use crate::core::vector2;
-use crate::{gui::{ Object, Rect, interface}, logic::GameState};
+use crate::{gui::interface, logic::GameState};
 fn main() {
     const TARGET_FPS: u64 = 120;
     let event_loop = event_loop::EventLoop::new();
     let mut facade = interface::Interface::create(&event_loop);
-
-
-    let mut obj = Object {
-        format: Rect {
-            size: vec2!(10., 10.),
-            center: vector2::ZERO,
-        },
-        color: [1., 1., 1.],
-    };
     let mut game_state = GameState::new(10, 10);
-    let mut last_start =   time::Instant::now();
-    let mut last_delta_time: u128 = 0;
+    let mut last_update =   time::Instant::now();
+    let mut last_key : Option<VirtualKeyCode> = None;  
     event_loop.run(move |ev, _, control_flow| {
         let start_time = time::Instant::now();
-
-        match ev {   
-            event::Event::WindowEvent { event, .. } => match event {
+        if let event::Event::WindowEvent { event, .. } = ev {
+            match event {
                 event::WindowEvent::CloseRequested => {
                     *control_flow = event_loop::ControlFlow::Exit;
                     return;
                 }
                 event::WindowEvent::Resized(window_size) => {
-                    facade
-                        .display
-                        .gl_window()
-                        .window();
                     facade.camera.target.size.x =
                         (window_size.height as f32) / (window_size.width as f32);
                 }
@@ -49,38 +35,37 @@ fn main() {
                     input:
                         KeyboardInput {
                             virtual_keycode: Some(input),
+                            state,
                             ..
                         },
                     ..
-                } =>{
-                    game_state.key_press(input);
-
-                    match input {
-                        event::VirtualKeyCode::Up => obj.format.center += vector2::UP,
-                        event::VirtualKeyCode::Down => obj.format.center += vector2::DOWN,
-                        event::VirtualKeyCode::Left => obj.format.center += vector2::LEFT,
-                        event::VirtualKeyCode::Right => obj.format.center += vector2::RIGHT,
-                        _ => (),
-                    }   
-                },    
-
+                } => {
+                    match state {
+                        ElementState::Pressed => {
+                            if last_key.is_none() || last_key.is_some_and(|k| k != input) {
+                                game_state.key_down(input);
+                            }
+                            last_key = Some(input);
+                        },
+                        ElementState::Released => {
+                            last_key = None;
+                        },
+                    }
+                 
+                },
                 _ => (),
-            },
-            
-            event::Event::RedrawRequested(_) => {
-                last_delta_time = time::Instant::now().duration_since(last_start).as_micros() as u128;
-                last_start = start_time;
-                let mut canvas = facade.draw();
-                canvas.target.clear_color(0.0, 0.0, 0.0, 1.0);
-                game_state.update(&mut canvas, last_delta_time);
-                canvas.draw_obj(&obj);
-                canvas.target.finish().unwrap();
             }
-            event::Event::RedrawEventsCleared => {
-                facade.display.gl_window().window().request_redraw();
-            }
-            _ => (),
         }
+
+        
+        let delta_t = time::Instant::now().duration_since(last_update).as_micros() as u128;
+        last_update = start_time;
+        let mut canvas = facade.draw();
+        canvas.target.clear_color(0.0, 0.0, 0.0, 1.0);
+        game_state.update(&mut canvas, delta_t);
+        canvas.target.finish().unwrap();
+
+
 
         let elapsed_time = time::Instant::now().duration_since(start_time).as_millis() as u64;
         let wait_millis = if 1000 / TARGET_FPS >= elapsed_time {
