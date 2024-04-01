86b266fc767cd09ef0d634abe88c9786358b6f26
GoliasVictor <dev.jvsb@gmail.com>
2024-04-01 01:58:33 -0300

    Added re-mvp

---
+++ re-mvp/engine/src/lib.rs
@@ -0,0 +1,10 @@
+//! This is a MVP of a modular game engine :D
+#![warn(missing_docs)]
+#![warn(clippy::missing_docs_in_private_items)]
+
+mod audio;
+mod colision;
+mod input;
+mod rendering;
+mod text_output;
+mod math;
